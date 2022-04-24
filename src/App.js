0b82a9e2222c93189098ffad7cb892bc3133c339
unknown <azeemolatunji@gmail.com>
2022-04-24 14:06:56 +0100

    update unbuttonsubmit funtion

--- src/App.js
+++ src/App.js
@@ -6,13 +6,28 @@ import { Component } from 'react';
 import Rank from './componets/Rank/rank';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+    input: ''
+  }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value)
+  }
+
+onButtonSubmit = () => {
+console.log("click")
+}
+
   render() {
     return (
       <div className="App">
         <Navigation />
         <Logo />
         <Rank />
-        < ImageLinkForm />
+        < ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
   
       </div>
     );
--- src/componets/imageLinkForm/imageLinkForm.js
+++ src/componets/imageLinkForm/imageLinkForm.js
@@ -1,6 +1,6 @@
 import './ImageLinkForm.css';
 
-function ImageLinkForm() {
+function ImageLinkForm({ onInputChange, onButtonSubmit }) {
   return (
     <div className="ma4">
       <p className='f3 center'>
@@ -8,8 +8,8 @@ function ImageLinkForm() {
       </p>
       <div className=' center' >
         <div className='form center pa3 br5 shadow-5'>
-          <input className='pa2 w-70' type= 'text'/>
-          <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-green'>Detect</button>
+          <input className='pa2 w-70' type= 'text' onChange= { onInputChange }/>
+          <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-green' onClick={onButtonSubmit}>Detect</button>
         </div>
       </div>
     </div>
